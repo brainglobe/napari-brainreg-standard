f55755752690b61dce9cff73e94086423e46819c
Adam Tyson <adam.tyson@ucl.ac.uk>
2020-08-20 14:44:21 +0100

    test

--- tests/test_brainreg_standard.py
+++ tests/test_brainreg_standard.py
@@ -8,6 +8,8 @@ data_dir = str(Path(__file__).resolve().parent / "data")
 def test_reader():
     # try to read data
     reader = napari_get_reader(str(data_dir))
+    print(str(data_dir))
+    print(reader)
     assert callable(reader)
 
     # make sure we're delivering the right format
